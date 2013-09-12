906e6a97df83647a23c2acb0ff44e5e90130b0e6
Ikuo Terado <eqobar@gmail.com>
2013-09-13 02:28:45 +0900

    Added own cookbooks

---
+++ site-cookbooks/unicorn/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: unicorn
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "unicorn" do
+  action :install
+end
